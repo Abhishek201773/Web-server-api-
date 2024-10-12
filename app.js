74abc12d13d9d6e9ab8229c6fb9f63fbe7aebb04
Abhishek Dixit <134839604+Abhishek201773@users.noreply.github.com>
2024-10-12 12:53:28 +0530

    Added Avtar
    Added avatar char  by api

--- app.js
+++ app.js
@@ -21,11 +21,24 @@ app.post('/post', async (req, res) => {
     const firstName = req.body.name;
     try {
         const response = await axios.get(`https://api.genderize.io/?name=${firstName}`);
+        let img;
+        let male=["Nolan","Kingston","Adrian","Jade","Brooklynn","Ryan","Liliana","Avery"];
+        let female=["Sawyer","Leo","Jocelyn","Wyatt","Easton","Christopher","Eliza","Amaya","Leah","Liam","Chase","Katherine"];
+        let random = Math.floor(Math.random() *7) + 1;
+        let ran = Math.floor(Math.random() *11) + 1;
+    
+        if(response.data.gender=="male"){
+            img=`https://api.dicebear.com/9.x/pixel-art/svg?seed=${male[random]}`;
+        }
+        else{
+            img=`https://api.dicebear.com/9.x/pixel-art/svg?seed=${female[ran]}`
+        }
         const data = {
             name: response.data.name,
             gender: response.data.gender,
             probability:response.data.probability,
             count:response.data.count,
+            img:img,
         };
         res.render('name.ejs', data);
     } 
